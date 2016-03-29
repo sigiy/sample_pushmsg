a653569d4714047385850cb20e292b945d52eaaf
shigihara <shigihara.tomonori@nck.co.jp>
2016-03-29 17:47:03 +0900

    no message

--- service-worker.js
+++ service-worker.js
@@ -3,9 +3,9 @@
 self.addEventListener('push', function(event) {
   console.log('Received a push message', event);
 
-  var title = 'Yay a message.';
+  var title = 'Gat a message.';
   var body = 'We have received a push message.';
-  var icon = '/images/icon-192x192.png';
+  var icon = '/images/icon@2x.png';
   var tag = 'simple-push-demo-notification-tag';
 
   event.waitUntil(
